ddba011f9de695ac6344f9ce0f6495634a3645eb
Elijah Greenholt <93458527+pingxian@users.noreply.github.com>
2023-10-21 02:18:36 +0800

    Update elmsign.sh

--- elmsign.sh
+++ elmsign.sh
@@ -6,7 +6,6 @@
 
 #pwd
 
-eval "rm -rf ./elmsign"
 
 _ftype=""
 get_arch=`arch`
@@ -43,7 +42,6 @@ else
         eval "chmod +x $PWD/sign-$_ftype"
         eval "$PWD/sign-$_ftype"
     else
-        cd elmsign
         echo "在$PWD/elmsign目录、$PWD目录下均未找到文件sign-$_ftype,尝试拉取远程仓库文件sign-$_ftype"
         download_elm
     fi
